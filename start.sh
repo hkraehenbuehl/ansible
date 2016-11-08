2af5b2b19b1097c6aa6ca5c1e3b40fb05b100e04
Hanspeter Kraehenbuehl <hanspeter.kraehenbuehl@isc-ejpd.admin.ch>
2016-11-08 17:56:33 +0100

    last start.sh mit neuem Server prod2

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@
 
 docker run -itd -h dev1 --env-file ~/docker/env.env -v /home/isc-krh/docker/ansible/shared_filesystem:/var/shared_filesystem -p 9999:80 -p 9998:8080 krh/ansible
 
-docker run -itd -h prod1 --network=private --ip=172.28.0.2 --env-file ~/docker/env.env -v /home/isc-krh/docker/ansible/shared_filesystem:/var/shared_filesystem krh/ansible
+docker run -itd -h prod1 --network=private --ip=172.28.0.2 --env-file /home/isc-krh/docker/env.env -v /home/isc-krh/docker/ansible/shared_filesystem:/var/shared_filesystem krh/ansible
+
+docker run -itd -h prod2 --network=private --ip=172.28.0.3 --env-file /home/isc-krh/docker/env.env -v /home/isc-krh/docker/ansible/shared_filesystem:/var/shared_filesystem krh/ansible
 
